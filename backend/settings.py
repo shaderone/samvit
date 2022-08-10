2ddf77d2ea7fb624b8b47592b0ea647f3ba495d1
VishDroid-dev <vk2345732@gmail.com>
2022-08-10 23:09:17 +0530

    added static dir

--- backend/settings.py
+++ backend/settings.py
@@ -133,6 +133,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
+STATIC_DIR = [
+    BASE_DIR / 'staticfile'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
