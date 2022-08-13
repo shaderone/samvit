43bbabecd8a6cde4b693215899b7211e60d63f76
pixiedust07 <sharonshaji127001@gmail.com>
2022-08-13 23:58:02 +0530

    refractor reservations-page

---
+++ lib/presentation/screens/reservation/widgets/reservation_card_widgets/card_middle_widget.dart
@@ -0,0 +1,107 @@
+import 'package:brechfete/core/constants.dart';
+import 'package:brechfete/presentation/screens/reservation/widgets/reservation_chip.dart';
+import 'package:flutter/material.dart';
+
+class CardMiddleNormalRow extends StatelessWidget {
+  const CardMiddleNormalRow({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text("Date"),
+            //replace with somthing more dynamic
+            SizedBox(height: 10),
+            Text(
+              "Tue, 22 september",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+        const ReservationChip(
+          chipCrossAxisAlignment: CrossAxisAlignment.start,
+          chipTitle: "Time",
+          chipText: "10:00 AM",
+          chipWidth: 110,
+          chipBgColor: primaryDarkShadeLight,
+          chipStrokeColor: strokeLight,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text("Slots"),
+            SizedBox(height: 10),
+            Text(
+              "300",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w900,
+                color: extraYellow,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class CardMiddleWrappedRow extends StatelessWidget {
+  const CardMiddleWrappedRow({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 20,
+      runSpacing: 10,
+      runAlignment: WrapAlignment.center,
+      crossAxisAlignment: WrapCrossAlignment.end,
+      alignment: WrapAlignment.center,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text("Date"),
+            //replace with somthing more dynamic
+            SizedBox(height: 10),
+            Text(
+              "Tue, 22 september",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text("Slots"),
+            SizedBox(height: 10),
+            Text(
+              "300",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w900,
+                color: extraYellow,
+              ),
+            ),
+          ],
+        ),
+        const ReservationChip(
+          chipCrossAxisAlignment: CrossAxisAlignment.center,
+          chipTitle: "Time",
+          chipText: "10:00 AM",
+          chipWidth: 110,
+          chipBgColor: primaryDarkShadeLight,
+          chipStrokeColor: strokeLight,
+        ),
+      ],
+    );
+  }
+}
