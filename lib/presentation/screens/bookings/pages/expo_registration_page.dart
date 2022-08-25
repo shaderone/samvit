f0ff20c23c514f949ccbb30dfd4525424a265177
pixiedust07 <sharonshaji127001@gmail.com>
2022-08-25 14:30:55 +0530

    Add sticky header to registration page

--- lib/presentation/screens/bookings/pages/expo_registration_page.dart
+++ lib/presentation/screens/bookings/pages/expo_registration_page.dart
@@ -8,6 +8,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:simple_gradient_text/simple_gradient_text.dart';
+import 'package:sticky_headers/sticky_headers/widget.dart';
 
 class ExpoRegistration extends StatelessWidget {
   const ExpoRegistration({
@@ -32,169 +33,232 @@ class ExpoRegistration extends StatelessWidget {
         ),
       ),
       body: SingleChildScrollView(
-        padding: const EdgeInsets.all(20),
         child: Form(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(height: 10),
-              GradientText(
-                'Please fill out the form to book',
-                style: GoogleFonts.ubuntu(
-                  fontSize: 18.0,
-                ),
-                colors: const [
-                  Color(0xFF6E6F71),
-                  Color(0xFFECECEC),
-                ],
-              ),
-              GradientText(
-                'Expo slots',
-                style: GoogleFonts.ubuntu(
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                ),
-                colors: const [
-                  Color(0xFF6E6F71),
-                  Color(0xFFECECEC),
-                ],
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                "INSTITUTION INFO",
-                style: TextStyle(
-                  color: secondaryBlueShadeLight,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                ),
-              ),
-              const Divider(
-                color: textWhiteShadeLight,
-                thickness: 1,
-              ),
-              CustomFormInput(
-                labelText: "Name",
-                textInputType: TextInputType.name,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "phone number is required";
-                  } else if (value.length != 10) {
-                    return "Enter a valid 10-digit phone number";
-                  }
-                  return null;
-                },
-                textInputAction: TextInputAction.next,
-                hintText: "Enter institution name",
-              ),
-              CustomFormInput(
-                labelText: "Email or phone",
-                textInputType: TextInputType.name,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "phone number is required";
-                  } else if (value.length != 10) {
-                    return "Enter a valid 10-digit phone number";
-                  }
-                  return null;
-                },
-                textInputAction: TextInputAction.next,
-                hintText: "Enter email or phone",
-              ),
-              CustomFormInput(
-                labelText: "Phone(optional)",
-                textInputType: TextInputType.name,
-                validator: (value) {
-                  //logic
-                  return null;
-                },
-                textInputAction: TextInputAction.next,
-                hintText: "Enter phone(optional)",
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                "FACULTY INFO",
-                style: TextStyle(
-                  color: secondaryBlueShadeLight,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                ),
-              ),
-              const Divider(
-                color: textWhiteShadeLight,
-                thickness: 1,
-              ),
-              CustomFormInput(
-                labelText: "Name",
-                textInputType: TextInputType.name,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "phone number is required";
-                  } else if (value.length != 10) {
-                    return "Enter a valid 10-digit phone number";
-                  }
-                  return null;
-                },
-                textInputAction: TextInputAction.next,
-                hintText: "Enter institution name",
-              ),
-              CustomFormInput(
-                labelText: "Email or phone",
-                textInputType: TextInputType.name,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "phone number is required";
-                  } else if (value.length != 10) {
-                    return "Enter a valid 10-digit phone number";
-                  }
-                  return null;
-                },
-                textInputAction: TextInputAction.next,
-                hintText: "Enter email or phone",
-              ),
-              CustomFormInput(
-                labelText: "Phone(optional)",
-                textInputType: TextInputType.name,
-                validator: (value) {
-                  //logic
-                  return null;
-                },
-                textInputAction: TextInputAction.next,
-                hintText: "Enter phone(optional)",
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SlotButton(
-                    buttonText: "Pay Later",
-                    onPressed: () {
-                      BookingScreen.isDateSelectedNotifier.value = false;
-                      BookingScreen.isTimeSelectedNotifier.value = false;
-                      Navigator.of(context).pushNamedAndRemoveUntil(
-                        App.bookingSuccessRoute,
-                        (route) => false,
-                      );
-                    },
-                    screenWidth: screenWidth,
+              StickyHeader(
+                header: Container(
+                  margin: const EdgeInsets.only(
+                    bottom: 20,
                   ),
-                  SlotButton(
-                    buttonText: "Pay Now",
-                    onPressed: () {
-                      BookingScreen.isDateSelectedNotifier.value = false;
-                      BookingScreen.isTimeSelectedNotifier.value = false;
-                      Navigator.of(context).push(
-                        CupertinoPageRoute(
-                          builder: (context) => const PayNowPage(),
+                  padding: const EdgeInsets.all(15.0),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: bottomNavDark,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: const Text(
+                    "INSTITUTION INFO",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: secondaryBlueShadeLight,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                content: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CustomFormInput(
+                        labelText: "Name",
+                        textInputType: TextInputType.name,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "phone number is required";
+                          } else if (value.length != 10) {
+                            return "Enter a valid 10-digit phone number";
+                          }
+                          return null;
+                        },
+                        textInputAction: TextInputAction.next,
+                        hintText: "Enter institution name",
+                      ),
+                      CustomFormInput(
+                        labelText: "Email",
+                        textInputType: TextInputType.name,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "phone number is required";
+                          } else if (value.length != 10) {
+                            return "Enter a valid 10-digit phone number";
+                          }
+                          return null;
+                        },
+                        textInputAction: TextInputAction.next,
+                        hintText: "Enter email",
+                      ),
+                      CustomFormInput(
+                        labelText: "Phone",
+                        textInputType: TextInputType.name,
+                        validator: (value) {
+                          //logic
+                          return null;
+                        },
+                        textInputAction: TextInputAction.next,
+                        hintText: "Enter phone",
+                      ),
+                      const SizedBox(height: 10),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    CustomFormInput(
+                      labelText: "Phone(other)",
+                      textInputType: TextInputType.name,
+                      validator: (value) {
+                        //logic
+                        return null;
+                      },
+                      textInputAction: TextInputAction.next,
+                      hintText: "Enter phone(other)",
+                    ),
+                    const SizedBox(height: 10),
+                  ],
+                ),
+              ),
+              StickyHeader(
+                header: Container(
+                  margin: const EdgeInsets.only(
+                    bottom: 20,
+                  ),
+                  padding: const EdgeInsets.all(15.0),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: bottomNavDark,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Column(
+                    children: const [
+                      Text(
+                        "FACULTY INFO",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: secondaryBlueShadeLight,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
                         ),
-                      );
-                    },
-                    bgColor: secondaryBlueShadeDark,
-                    screenWidth: screenWidth,
+                      ),
+                      Text(
+                        "(contact person from institution)",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: textWhiteShadeLight,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
+                content: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 10),
+                      CustomFormInput(
+                        labelText: "Name",
+                        textInputType: TextInputType.name,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "phone number is required";
+                          } else if (value.length != 10) {
+                            return "Enter a valid 10-digit phone number";
+                          }
+                          return null;
+                        },
+                        textInputAction: TextInputAction.next,
+                        hintText: "Enter Faculty name",
+                      ),
+                      const SizedBox(height: 10),
+                      CustomFormInput(
+                        labelText: "Email",
+                        textInputType: TextInputType.name,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "phone number is required";
+                          } else if (value.length != 10) {
+                            return "Enter a valid 10-digit phone number";
+                          }
+                          return null;
+                        },
+                        textInputAction: TextInputAction.next,
+                        hintText: "Enter email",
+                      ),
+                      CustomFormInput(
+                        labelText: "Phone",
+                        textInputType: TextInputType.name,
+                        validator: (value) {
+                          //logic
+                          return null;
+                        },
+                        textInputAction: TextInputAction.next,
+                        hintText: "Enter phone",
+                      ),
+                      const SizedBox(height: 10),
+                    ],
+                  ),
+                ),
               ),
-              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    CustomFormInput(
+                      labelText: "Phone(other)",
+                      textInputType: TextInputType.name,
+                      validator: (value) {
+                        //logic
+                        return null;
+                      },
+                      textInputAction: TextInputAction.next,
+                      hintText: "Enter phone(other)",
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SlotButton(
+                          buttonText: "Pay Later",
+                          onPressed: () {
+                            BookingScreen.isDateSelectedNotifier.value = false;
+                            BookingScreen.isTimeSelectedNotifier.value = false;
+                            Navigator.of(context).pushNamedAndRemoveUntil(
+                              App.bookingSuccessRoute,
+                              (route) => false,
+                            );
+                          },
+                          screenWidth: screenWidth,
+                        ),
+                        SlotButton(
+                          buttonText: "Pay Now",
+                          onPressed: () {
+                            BookingScreen.isDateSelectedNotifier.value = false;
+                            BookingScreen.isTimeSelectedNotifier.value = false;
+                            Navigator.of(context).push(
+                              CupertinoPageRoute(
+                                builder: (context) => const PayNowPage(),
+                              ),
+                            );
+                          },
+                          bgColor: secondaryBlueShadeDark,
+                          screenWidth: screenWidth,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 30),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
