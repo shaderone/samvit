c5f12676a4ad309199dfb4907b4c9b761afab0a7
pixiedust07 <sharonshaji127001@gmail.com>
2022-08-27 20:47:42 +0530

    Refactor registration form buttons

--- lib/presentation/screens/bookings/pages/expo_registration_page.dart
+++ lib/presentation/screens/bookings/pages/expo_registration_page.dart
@@ -1,10 +1,6 @@
 import 'package:brechfete/core/constants.dart';
-import 'package:brechfete/presentation/root/app.dart';
-import 'package:brechfete/presentation/screens/bookings/booking_screen.dart';
-import 'package:brechfete/presentation/screens/bookings/pages/pay_now_page.dart';
-import 'package:brechfete/presentation/screens/bookings/pages/widgets/registration_buttons.dart';
+import 'package:brechfete/presentation/screens/bookings/pages/widgets/registration_form_buttons.dart';
 import 'package:brechfete/presentation/screens/bookings/pages/widgets/registration_form_holder.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:simple_gradient_text/simple_gradient_text.dart';
@@ -56,47 +52,8 @@ class ExpoRegistration extends StatelessWidget {
                   isInstitution: false,
                   suffixIcon: MdiIcons.phoneClassic,
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      RegistrationButton(
-                        buttonText: "Pay Later",
-                        onPressed: () {
-                          if (formKey1.currentState!.validate()) {
-                            formKey1.currentState!.save();
-                            BookingScreen.isDateSelectedNotifier.value = false;
-                            BookingScreen.isTimeSelectedNotifier.value = false;
-                            Navigator.of(context).pushNamedAndRemoveUntil(
-                              App.bookingSuccessRoute,
-                              (route) => false,
-                            );
-                          }
-                        },
-                        screenWidth: screenWidth,
-                      ),
-                      RegistrationButton(
-                        buttonText: "Pay Now",
-                        onPressed: () {
-                          if (formKey1.currentState!.validate()) {
-                            formKey1.currentState!.save();
-                            BookingScreen.isDateSelectedNotifier.value = false;
-                            BookingScreen.isTimeSelectedNotifier.value = false;
-                            Navigator.of(context).push(
-                              CupertinoPageRoute(
-                                builder: (context) => const PayNowPage(),
-                              ),
-                            );
-                          }
-                        },
-                        bgColor: secondaryBlueShadeDark,
-                        screenWidth: screenWidth,
-                      ),
-                    ],
-                  ),
-                ),
+                RegistrationFormButtons(
+                    formKey1: formKey1, screenWidth: screenWidth),
                 const SizedBox(height: 100),
               ],
             ),
@@ -106,47 +63,48 @@ class ExpoRegistration extends StatelessWidget {
     );
   }
 
-  //phone validator, email validator, name validator
-
-  Future<bool?> shouldPopScreen(BuildContext context) => showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          backgroundColor: primaryDark,
-          title: const Text("Discard changes?"),
-          titleTextStyle: const TextStyle(fontSize: 20),
-          content: const Text("Any changes made will be lost"),
-          contentTextStyle: const TextStyle(color: textWhiteShadeLight),
-          contentPadding:
-              const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
-          actionsPadding: const EdgeInsets.only(right: 10),
-          buttonPadding:
-              const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(false),
-              child: const Text(
-                "No",
-                style: TextStyle(
-                  color: secondaryBlueShadeLight,
-                ),
-              ),
-            ),
-            OutlinedButton(
-              onPressed: () => Navigator.of(context).pop(true),
-              style: ButtonStyle(
-                side: MaterialStateProperty.all(
-                  const BorderSide(color: extraRed),
-                ),
-                backgroundColor: MaterialStateProperty.all(primaryDark),
-              ),
-              child: const Text(
-                "Discard",
-                style: TextStyle(
-                  color: pureWhite,
-                ),
-              ),
-            ),
-          ],
+  Future<bool?> shouldPopScreen(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        backgroundColor: primaryDark,
+        title: const Text("Discard changes?"),
+        titleTextStyle: const TextStyle(fontSize: 20),
+        content: const Text("Any changes made will be lost"),
+        contentTextStyle: const TextStyle(color: textWhiteShadeLight),
+        contentPadding: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+        actionsPadding: const EdgeInsets.only(right: 10),
+        buttonPadding: const EdgeInsets.symmetric(
+          vertical: 5,
+          horizontal: 10,
         ),
-      );
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: const Text(
+              "No",
+              style: TextStyle(
+                color: secondaryBlueShadeLight,
+              ),
+            ),
+          ),
+          OutlinedButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            style: ButtonStyle(
+              side: MaterialStateProperty.all(
+                const BorderSide(color: extraRed),
+              ),
+              backgroundColor: MaterialStateProperty.all(primaryDark),
+            ),
+            child: const Text(
+              "Discard",
+              style: TextStyle(
+                color: pureWhite,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
