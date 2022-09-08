aac8066aaf7dfd18bbe24ff2a24b54d574ffc1d0
pixiedust07 <sharonshaji127001@gmail.com>
2022-09-08 23:11:47 +0530

    Show amount to pay on Ready-cash page

--- lib/presentation/screens/bookings/pages/pay_now_page.dart
+++ lib/presentation/screens/bookings/pages/pay_now_page.dart
@@ -30,8 +30,32 @@ class _PayNowPageState extends State<PayNowPage> {
   bool isQrCodeGenerated = false;
   String qrData = "";
   int? amount;
+  int? amountToPay;
+  String? slotCount;
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      //send request
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      final token = prefs.getString("token");
+      final regId = prefs.getString("regToken");
+      final selectedSlot = prefs.getString("slotCount");
+      var client = http.Client();
+      var response = await client.get(
+        Uri.parse(
+          "$baseURL/amount/$regId",
+        ),
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Token $token"
+        },
+      );
+      final data = jsonDecode(response.body);
+      setState(() {
+        amountToPay = data['amount'];
+        slotCount = selectedSlot;
+      });
+    });
     final screenWidth = MediaQuery.of(context).size.width;
     return DefaultTabController(
       length: 2,
@@ -84,47 +108,47 @@ class _PayNowPageState extends State<PayNowPage> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      //Row(
-                      //  children: [
-                      //    Expanded(
-                      //      child: Container(
-                      //        decoration: BoxDecoration(
-                      //          color: extraYellow.withOpacity(.05),
-                      //          borderRadius: BorderRadius.circular(5),
-                      //          border: Border.all(
-                      //            color: extraYellow.withOpacity(.5),
-                      //          ),
-                      //        ),
-                      //        child: Padding(
-                      //          padding: const EdgeInsets.all(15.0),
-                      //          child: RichText(
-                      //            textAlign: TextAlign.center,
-                      //            text: TextSpan(
-                      //              style: DefaultTextStyle.of(context).style,
-                      //              children: const [
-                      //                TextSpan(
-                      //                  text:
-                      //                      "Amount to Pay : 20 x 40 (slots) = ",
-                      //                  style: TextStyle(
-                      //                    fontSize: 14,
-                      //                    color: textWhiteShadeLight,
-                      //                  ),
-                      //                ),
-                      //                TextSpan(
-                      //                  text: "800 ₹",
-                      //                  style: TextStyle(
-                      //                    fontSize: 16,
-                      //                    fontWeight: FontWeight.bold,
-                      //                  ),
-                      //                ),
-                      //              ],
-                      //            ),
-                      //          ),
-                      //        ),
-                      //      ),
-                      //    ),
-                      //  ],
-                      //),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: extraYellow.withOpacity(.05),
+                                borderRadius: BorderRadius.circular(5),
+                                border: Border.all(
+                                  color: extraYellow.withOpacity(.5),
+                                ),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(15.0),
+                                child: RichText(
+                                  textAlign: TextAlign.center,
+                                  text: TextSpan(
+                                    style: DefaultTextStyle.of(context).style,
+                                    children: [
+                                      TextSpan(
+                                        text:
+                                            "Amount to Pay [$slotCount slots] = ",
+                                        style: const TextStyle(
+                                          fontSize: 14,
+                                          color: textWhiteShadeLight,
+                                        ),
+                                      ),
+                                      TextSpan(
+                                        text: "${amountToPay.toString()} ₹",
+                                        style: const TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                       const SizedBox(height: 20),
                       Column(
                         mainAxisSize: MainAxisSize.max,
