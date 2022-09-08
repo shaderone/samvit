4b009cb1b244180dd655e294c963e9d408384047
pixiedust07 <sharonshaji127001@gmail.com>
2022-09-08 14:03:37 +0530

    update payment history ui

--- lib/presentation/screens/payments/payment_history_screen.dart
+++ lib/presentation/screens/payments/payment_history_screen.dart
@@ -1,10 +1,10 @@
 import 'package:brechfete/core/constants.dart';
 import 'package:brechfete/presentation/screens/bookings/booking_screen.dart';
 import 'package:brechfete/presentation/screens/reservations/widgets/reservation_chip.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:simple_gradient_text/simple_gradient_text.dart';
-import 'package:tabbed_sliverlist/tabbed_sliverlist.dart';
 
 class PaymentHistoryScreen extends StatefulWidget {
   const PaymentHistoryScreen({Key? key}) : super(key: key);
@@ -17,153 +17,136 @@ class _PaymentHistoryScreenState extends State<PaymentHistoryScreen> {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: TabbedList(
-        tabLength: 3,
-        sliverTabBar: SliverTabBar(
-          title: Row(
-            children: [
-              Image.asset("assets/images/samvit_logo.png", width: 30),
-              const SizedBox(width: 15),
-              GradientText(
-                'Payment History',
-                style: GoogleFonts.ubuntu(
-                  fontSize: screenWidth <= 320 ? 20 : 24,
+    return DefaultTabController(
+      length: 3,
+      initialIndex: 0,
+      child: Scaffold(
+        body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                title: Row(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.asset("assets/images/samvit_logo.png",
+                          width: 30),
+                    ),
+                    const SizedBox(width: 15),
+                    GradientText(
+                      'Payment History',
+                      style: GoogleFonts.ubuntu(
+                        fontSize: screenWidth <= 320 ? 20 : 24,
+                      ),
+                      colors: const [
+                        Color(0xFF6E6F71),
+                        Color(0xFFECECEC),
+                      ],
+                    ),
+                  ],
                 ),
-                colors: const [
-                  Color(0xFF6E6F71),
-                  Color(0xFFECECEC),
+                pinned: true,
+                floating: true,
+                actions: [
+                  IconButton(
+                    onPressed: () => logout(mounted, context),
+                    icon: const Icon(
+                      Icons.logout_rounded,
+                    ),
+                  ),
                 ],
-              ),
-            ],
-          ),
-          actions: [
-            IconButton(
-              onPressed: () => logout(mounted, context),
-              icon: const Icon(
-                Icons.logout_rounded,
-              ),
-            ),
-          ],
-          tabBar: TabBar(
-            indicatorColor: secondaryBlueShadeLight,
-            tabs: [
-              const Tab(
-                text: 'Online',
-              ),
-              Tab(
-                child: Text(
-                  "Ready Cash",
-                  style: TextStyle(fontSize: screenWidth <= 320 ? 12 : 16),
+                bottom: TabBar(
+                  indicatorColor: secondaryBlueShadeLight,
+                  isScrollable: screenWidth < 340 ? true : false,
+                  tabs: const [
+                    Tab(child: Text('Ready Cash')),
+                    Tab(child: Text('Online')),
+                    Tab(child: Text('pay Later')),
+                  ],
                 ),
               ),
-              const Tab(
-                text: 'Pay later',
-              )
-            ],
+            ];
+          },
+          body: TabBarView(
+            dragStartBehavior: DragStartBehavior.down,
+            physics: const NeverScrollableScrollPhysics(),
+            children: List.generate(
+              3,
+              (index) {
+                return Padding(
+                  padding: const EdgeInsets.only(top: 3),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        children: [
+                          ColoredBox(
+                            color: primaryDark,
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 15),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "Name",
+                                    style: GoogleFonts.ubuntu(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 17,
+                                    ),
+                                  ),
+                                  Text(
+                                    "Status",
+                                    style: GoogleFonts.ubuntu(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 17,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: ListView.separated(
+                            itemBuilder: (BuildContext context, int index) {
+                              return Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  const Text("St. Peters"),
+                                  ReservationChip(
+                                    chipCrossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    chipTitle: "",
+                                    chipText: "Processing",
+                                    chipWidth: 120,
+                                    chipBgColor:
+                                        secondaryBlueShadeDark.withOpacity(.1),
+                                    chipStrokeColor: secondaryBlueShadeDark,
+                                    isPaymentChip: true,
+                                  ),
+                                ],
+                              );
+                            },
+                            separatorBuilder:
+                                (BuildContext context, int index) =>
+                                    const Divider(),
+                            itemCount: 20,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
           ),
         ),
-        tabLists: [
-          TabListBuilder(
-            uniquePageKey: 'page1',
-            length: 15,
-            builder: (BuildContext context, index) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ListTile(
-                      leading: const Text(
-                        "St.Joseph Hss",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                      ),
-                      trailing: ReservationChip(
-                        chipCrossAxisAlignment: CrossAxisAlignment.center,
-                        chipTitle: "",
-                        chipText: "PAID",
-                        chipWidth: 110,
-                        chipBgColor: extraGreen.withOpacity(0.1),
-                        chipStrokeColor: extraGreen,
-                        isPaymentChip: true,
-                      ),
-                    ),
-                    const Divider(thickness: 2)
-                  ],
-                ),
-              );
-            },
-          ),
-          TabListBuilder(
-            uniquePageKey: 'page2',
-            length: 5,
-            builder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ListTile(
-                      leading: const Text(
-                        "St.Joseph Hss",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                      ),
-                      trailing: ReservationChip(
-                        chipCrossAxisAlignment: CrossAxisAlignment.center,
-                        chipTitle: "",
-                        chipText: "PENDING",
-                        chipWidth: 110,
-                        chipBgColor: extraYellow.withOpacity(0.1),
-                        chipStrokeColor: extraYellow,
-                        isPaymentChip: true,
-                      ),
-                    ),
-                    const Divider(thickness: 2)
-                  ],
-                ),
-              );
-            },
-          ),
-          TabListBuilder(
-            uniquePageKey: 'page3',
-            length: 3,
-            builder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ListTile(
-                      leading: const Text(
-                        "St.Joseph Hss",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                      ),
-                      trailing: ReservationChip(
-                        chipCrossAxisAlignment: CrossAxisAlignment.center,
-                        chipTitle: "",
-                        chipText: "PROCESSING",
-                        chipWidth: 130,
-                        chipBgColor: secondaryBlueShadeDark.withOpacity(0.1),
-                        chipStrokeColor: secondaryBlueShadeDark,
-                        isPaymentChip: true,
-                      ),
-                    ),
-                    const Divider(thickness: 2)
-                  ],
-                ),
-              );
-            },
-          )
-        ],
       ),
     );
   }
